6f2c875f2c0cec8b97aba2247851eef732d51ca8
ManhProduce <thieusymanh2001@gmail.com>
2025-04-08 18:26:49 +0700

    config validation class, hash password and create user update in DB

--- 02-nest-rn/src/modules/users/dto/create-user.dto.ts
+++ 02-nest-rn/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty({ message: "name không được để trống" })
+    name: string;
+
+    @IsNotEmpty({ message: "email không được để trống" })
+    @IsEmail({}, { message: 'Invalid email message' })
+    email: string;
+
+    @IsNotEmpty({ message: "password không được để trống" })
+    password: string;
+
+
+    phone: string;
+    address: string;
+    image: string;
+}

69593a70e0e1cf16a4ebe8de56216e189a7b748e
ManhProduce <thieusymanh2001@gmail.com>
2025-04-14 16:57:18 +0700

    create accessToken

--- 02-nest-rn/src/auth/dto/create-auth.dto.ts
+++ 02-nest-rn/src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAuthDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateAuthDto {
+    @IsNotEmpty({ message: "username không được để trống" })
+    username: string;
+
+    @IsNotEmpty({ message: "password không được để trống" })
+    password: string
+}
